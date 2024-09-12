14c8e9dc9a77271bdac1966ee86f7fb09e63000f
Linkan42 <sali20jt@student.ju.se>
2024-09-12 14:48:02 +0200

    FIX: simplified fitness calculation. added score attribute to genome class

--- src/evolutionary_classes/fitness.py
+++ src/evolutionary_classes/fitness.py
@@ -2,7 +2,6 @@
 Module containing static fitness function(s)
 """
 
-import data
 from genome import Genome
 
 class Fitness:
@@ -10,7 +9,7 @@ class Fitness:
     Function(s) that calculates fitness of a Genome
     """
     @staticmethod
-    def calc_fitness(genome: Genome) -> int:
+    def calc_fitness(genome: Genome) -> None:
         """
         Calculate the fitness score of a genome
         """
@@ -18,18 +17,6 @@ class Fitness:
 
         for person in genome.people:
             if person.has_arrived:
-                # Reward based on:
-                # More people
-                accumulated_score += data.NUMBER_OF_PEOPLE
-                # More floors
-                accumulated_score += data.NUMBER_OF_FLOORS
-                # Less stops
-                accumulated_score += int(round(10.0 * float(1.0 / float(data.GENOME_LENGTH))))
+                accumulated_score += 10
 
-            # Punish based on:
-            # Excess travel
-            delta_distance = person.distance_traveled - person.distance_needed
-            if delta_distance > 0:
-                accumulated_score -= delta_distance
-
-        return accumulated_score
+        genome.fitness_score = accumulated_score
