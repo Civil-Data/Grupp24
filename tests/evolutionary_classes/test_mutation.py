f8dead267df2cf77f7f02e5fa0227f3d7012a44c
joelScarinius <anjo19go@student.ju.se>
2024-09-17 15:30:54 +0200

    FIX: Added some extra checks

--- tests/evolutionary_classes/test_mutation.py
+++ tests/evolutionary_classes/test_mutation.py
@@ -12,30 +12,36 @@ sys.path.insert(
 
 from genome import Genome
 from evolutionary_classes.mutation import Mutation
+from data import NUMBER_OF_FLOORS
+
+genome = Genome([])
+
 
 def test_swap() -> None:
     """
     Test that the swap function actually swaps two random indices in the genome.
     """
-    genome = Genome([1, 2, 3, 4, 5, 6])
+    genome.genome = [1, 2, 3, 4, 5, 6]
     Mutation.swap(genome)
     assert genome.genome != [1, 2, 3, 4, 5, 6]
+    assert len(genome.genome) == 6
 
 
 def test_increase_genome_length() -> None:
     """
     Test that the increase_genome_length function actually increases the length of the genome.
     """
-    genome = Genome([1, 2, 3, 4, 5, 6])
+    genome.genome = [1, 2, 3, 4, 5, 6]
     Mutation.increase_genome_length(genome)
     assert len(genome.genome) == 7
+    assert 0 <= genome.genome[6] < NUMBER_OF_FLOORS
 
 
 def test_decrease_genome_length() -> None:
     """
     Test that the decrease_genome_length function actually decreases the length of the genome.
     """
-    genome = Genome([1, 2, 3, 4, 5, 6])
+    genome.genome = [1, 2, 3, 4, 5, 6]
     Mutation.decrease_genome_length(genome)
     assert len(genome.genome) == 5
 
@@ -44,17 +50,18 @@ def test_swap_and_inc() -> None:
     """
     Test that the swap_and_inc function actually swaps two random indices in the genome and increases the length of the genome.
     """
-    genome = Genome([1, 2, 3, 4, 5, 6])
+    genome.genome = [1, 2, 3, 4, 5, 6]
     Mutation.swap_and_inc(genome)
     assert genome.genome != [1, 2, 3, 4, 5, 6]
     assert len(genome.genome) == 7
+    assert 0 <= genome.genome[6] < NUMBER_OF_FLOORS
 
 
 def test_swap_and_dec() -> None:
     """
     Test that the swap_and_dec function actually swaps two random indices in the genome and decreases the length of the genome.
     """
-    genome = Genome([1, 2, 3, 4, 5, 6])
+    genome.genome = [1, 2, 3, 4, 5, 6]
     Mutation.swap_and_dec(genome)
     assert genome.genome != [1, 2, 3, 4, 5, 6]
     assert len(genome.genome) == 5
