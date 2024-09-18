92e61e944da93b0608bc7f820870e7aabd3e8c49
Gustav <gustavpra@gmail.com>
2024-09-19 01:56:10 +0200

    Added: Functions to run experiments from .json files

---
+++ src/experiment/create_experiment.py
@@ -0,0 +1,92 @@
+"""
+Function to created random generation of genomes
+Saved to text file for now. For multiple testing purposes.
+"""
+
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
+import json
+import random
+import data
+from main_classes.person import Person
+
+
+# Number of genomes in generation
+number_of_genomes = 1000
+# Length range on genomes
+range_start = 12
+range_end = 14
+# numbers of floors
+floors = 12
+# How many people can be slotted on one floor
+floor_length = 27
+
+
+def create_random_generation(
+    number_of_genomes,
+    range_start,
+    range_end,
+    number_of_floors,
+    filename=f"Generation_{number_of_genomes}.json",
+) -> data.Population:
+
+    population_list: data.Population = []
+
+    for _ in range(number_of_genomes):
+        k = random.randint(range_start, range_end)
+
+        # set start to floor 0
+        genome_list: data.Genome = [0]
+        # used if first number doesn't need to be 0
+        # genome_list = [random.choice(range(m + 1))]
+
+        # Ensure not the same floor two times in a row
+        for _ in range(k - 1):
+            next_num = random.choice(range(number_of_floors + 1))
+            while next_num == genome_list[-1]:
+                next_num = random.choice(range(number_of_floors + 1))
+
+            genome_list.append(next_num)
+
+        population_list.append(genome_list)
+
+        with open(filename, "w") as file:
+            json.dump(population_list, file, indent=4)
+    # To ensure success
+    return population_list
+
+
+def create_random_building(
+    number_of_floor,
+    floor_queue_length,
+    filename=f"Building_{floors}_{floor_length}.json",
+) -> data.People:
+    building_list: data.People = []
+
+    for i in range(number_of_floor):
+
+        for _ in range(floor_queue_length):
+
+            end_floor = random.choice(range(number_of_floor))
+
+            while i == end_floor:
+                end_floor = random.choice(range(number_of_floor))
+
+            building_list.append(Person(i, end_floor))
+
+    with open(filename, "w") as file:
+        json.dump(
+            [person.to_json() for person in building_list],
+            file,
+            indent=4,
+        )
+
+
+create_random_generation(number_of_genomes, range_start, range_end, floors)
+create_random_building(floors, floor_length)
+
+print(
+    f"List of {number_of_genomes} generations has been created check file for results!"
+)
---
+++ src/experiment/experiment.py
@@ -0,0 +1,74 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
+
+import json
+import data
+import matplotlib.pyplot as plt
+
+from evolutionary_classes.fitness import Fitness
+from evolutionary_classes.populate import Populate
+from evolutionary_classes.selection import Selection
+from evolutionary_classes.crossover import Crossover
+from evolutionary_classes.mutation import Mutation
+from main_classes.person import Person
+from genome import Genome
+
+
+class ExperimentElevator:
+
+    def __init__(self, people_path, generation_path) -> None:
+        self.people_list: data.People_queues = people_path
+        self.generation_list: data.Population = generation_path
+
+    def run(self):
+        results = main.run_evolution(
+            populate_function=Populate.generate_population,
+            fitness_function=Fitness.calc_fitness,
+            selection_function=Selection.rank,
+            crossover_function=Crossover.swap_last_halves,
+            mutation_functions=[Mutation.swap, Mutation.increase_genome_length],
+            experiment=self,
+        )
+
+    def display_experiment(self, name, results) -> None:
+        generation = [res[0] for res in results]
+        fitness_score = [res[1] for res in results]
+        genome_length = [res[2] for res in results]
+        plt.plot(generation, fitness_score, label="Fitness Score", color="blue")
+        plt.plot(generation, genome_length, label="Genome Length", color="green")
+        plt.title(f"Experiment : {name}")
+        plt.xlabel("Generation")
+        plt.ylabel("Value")
+        plt.legend()
+        # Show experiment graph after every run
+        # plt.show()
+
+
+def load_population(filename) -> data.Population:
+    """
+    Loading in data from a json file
+    """
+    with open(filename, "r") as file:
+        data = json.load(file)
+
+    genome_list = [Genome(genome_data) for genome_data in data]
+
+    return genome_list
+
+
+def load_building(filename) -> data.People:
+    with open(filename, "r") as file:
+        data = json.load(file)
+
+    building_list = [Person.from_json(person_data) for person_data in data]
+
+    return building_list
+
+
+def save_to_excel(results, output_file):
+    """
+    Function to save results to external output
+    """
+    data = []
