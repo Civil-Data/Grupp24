5aaa72f7d70147757a4fcfab2d13b31f26938400
Linkan42 <sali20jt@student.ju.se>
2024-09-24 00:22:17 +0200

    REFACTOR: moved experiment variables to data.py

--- src/data.py
+++ src/data.py
@@ -4,25 +4,30 @@ from typing import List, Callable, Tuple
 from genome import Genome, Person
 
 NUMBER_OF_FLOORS: int = 15
-NUMBER_OF_PEOPLE: int = NUMBER_OF_FLOORS * 1
+NUMBER_OF_PEOPLE: int = NUMBER_OF_FLOORS * 3
 
 ELEVATOR_CAPACITY: int = 8
 
 GENOME_LENGTH: int = NUMBER_OF_FLOORS
 POPULATION_SIZE: int = 100
-GENERATION_LIMIT: int = 100000
+GENERATION_LIMIT: int = 1000
 
-MUTATION_CHANCE: float = 0.05
+MUTATION_CHANCE: float = 0.2
 CROSSOVER_CHANCE: float = 1.0
-
-PERSON_ARRIVED_SCORE: int = 10
-MAXIMUM_POSSIBLE_SCORE: int = NUMBER_OF_PEOPLE * PERSON_ARRIVED_SCORE
+# Top % of genomes to directly carry over to the next generation
+ELITISM_PERC: float = 0.0
 
 # How much extra time a person who doesn't end up on the right floor gives
 TIME_PENALTY: int = 100
 
-# Top % of genomes to directly carry over to the next generation
-ELITISM_PERC: float = 0.0
+# Whether or not to use the experiment library by Gurra
+DO_EXP: bool = True
+
+EXP_RANDOM_BUILDING: bool = False
+EXP_RANGE_START = GENOME_LENGTH // 2
+EXP_RANGE_END = GENOME_LENGTH
+# How many people can be slotted on one floor. Only matters for even buildings
+EXP_FLOOR_LENGTH = 3
 
 Population = List[Genome]
 
