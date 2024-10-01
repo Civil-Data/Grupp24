56100352f778933a069a242ca556d019e4d1a4a7
joelScarinius <anjo19go@student.ju.se>
2024-10-01 11:10:46 +0200

    FIX: Header in most files

--- tests/evolutionary_classes/test_fitness.py
+++ tests/evolutionary_classes/test_fitness.py
@@ -1,6 +1,5 @@
 # Author: Joel Scarinius Stävmo, Oskar Sundberg, Linus Savinainen, Samuel Wallander Leyonberg  and Gustav Pråmell
 # Update: October 1, 2024
-# Description: " "
 # Version: 1.0.0
 # License: Apache 2.0
 
